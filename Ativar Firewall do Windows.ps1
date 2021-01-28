5e85b5e1a501878c18e11e80de5245c69281a5be
Daniel Neo <78002139+danielneo27@users.noreply.github.com>
2021-01-28 17:08:21 -0300

    Add files via upload

---
+++ Ativar Firewall do Windows.ps1
@@ -0,0 +1,6 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+netsh.exe advfirewall set allprofiles state on
+Write-Host "Firewall Ativado" -F Yellow
+
+netsh.exe advfirewall show allprofiles state
+timeout /t 10
---
+++ Desativar Firewall do Windows.ps1
@@ -0,0 +1,6 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+netsh.exe advfirewall set allprofiles state off
+Write-Host "Firewall Desativado" -F Yellow
+
+netsh.exe advfirewall show allprofiles state
+timeout /t 5
