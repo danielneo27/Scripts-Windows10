eafa958d0c749c61c5a7f382e523586fdb8188e6
Daniel Neo <78002139+danielneo27@users.noreply.github.com>
2021-01-28 17:12:33 -0300

    Add files via upload

---
+++ Tocar-musica.ps1
@@ -0,0 +1,14 @@
+﻿if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+
+add-type -AssemblyName PresentationCore
+
+$music = New-Object System.Windows.Media.MediaPlayer
+
+$music.Open("C:\Users\danie\Downloads\Samm Henshaw - The World Is Mine (Official Video).mp3")
+$music.Volume = 0.2
+
+$music.Play()
+
+Start-Sleep -Seconds 15
+
+$music.Stop()
